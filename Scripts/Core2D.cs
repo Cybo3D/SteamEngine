52fec55561844e427d7bfb69c582f239ead4c78e
Cybo3D <technoboykaj@gmail.com>
2023-11-25 19:14:35 +0100

    added middle position to the core2d changed somethings to vector2

--- Scripts/Core2D.cs
+++ Scripts/Core2D.cs
@@ -6,18 +6,31 @@ namespace SteamEngine
 	public class Core2D : Core
 	{
         public Vector3 Position;
-        public Vector3 Size;
+        public Vector2 Size = new Vector2(1, 1);
         public Vector3 Rotation;
-		public Core2D()
+        public Vector3 middlePosition;
+        public Core2D(Vector3 position)
 		{
-			
+			Position = position;
+            middlePosition = Position / new Vector3(Size, Position.Z);
 		}
-
+        public Core2D(Vector3 position, Vector3 rotation)
+		{
+			Position = position;
+            Rotation = rotation;
+            middlePosition = Position / new Vector3(Size, Position.Z);
+		}
+		public Core2D(Vector3 position, Vector3 rotation, Vector2 size)
+		{
+			Position = position;
+            Rotation = rotation;
+            Size = size;
+            middlePosition = Position / new Vector3(Size, Position.Z);
+		}
 		public override void Update(GameTime gameTime)
 		{
 
 		}
-
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
 		{
 			
