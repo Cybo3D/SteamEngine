644924f760272f9488e620ba57f4071921d681e5
Cybo3D <technoboykaj@gmail.com>
2023-12-07 21:05:29 +0100

    fixed some bugs lol

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -13,6 +15,7 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     WaterInput input = new WaterInput();
+    Cluster player;
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -33,6 +36,14 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        player = new Cluster(new List<Core2D>{
+            new Sprite2D(Content.Load<Texture2D>("SpaceShip")){
+                LocalPosition = new Vector3(10, 0 ,0)
+            },
+            new Sprite2D(Content.Load<Texture2D>("SpaceShip")){
+                LocalPosition = new Vector3(0, 0 ,0)
+            }
+        });
         // TODO: use this.Content to load your game content here
     }
 
@@ -53,6 +64,7 @@ public class Game1 : Game
         
         _spriteBatch.Begin();
         // TODO: Add your drawing code here
+        player.Draw(gameTime, _spriteBatch);
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- Scripts/Cluster.cs
+++ Scripts/Cluster.cs
@@ -5,47 +5,56 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SteamEngine
 {
-    public class Cluster : Core2D{
+    public class Cluster : Core2D
+    {
         public List<Core2D> Cores;
-        public Cluster(List<Core2D> clusters){
+        public Cluster(List<Core2D> clusters)
+        {
             Cores = clusters;
-            foreach(var Core in Cores){
+            foreach (var Core in Cores)
+            {
                 //Core.Update(gameTime);
 
-                if(Core == Cores[0]){
+                if (Core == Cores[0])
+                {
                     Core.GlobalPosition = Core.LocalPosition;
                     Core.GlobalRotation = Core.LocalRotation;
                     Core.GlobalSize = Core.LocalSize;
                 }
-
-                Core.GlobalPosition = Cores[0].LocalPosition + Core.LocalPosition;
-                Core.GlobalRotation += Cores[0].LocalRotation + Core.LocalRotation;
-                Core.GlobalSize += Cores[0].LocalSize + Core.LocalSize;
+                else
+                {
+                    Core.GlobalPosition = Cores[0].LocalPosition + Core.LocalPosition;
+                    Core.GlobalRotation = Cores[0].LocalRotation + Core.LocalRotation;
+                    Core.GlobalSize = Cores[0].LocalSize + Core.LocalSize;
+                }
             }
         }
         public override void Update(GameTime gameTime)
-		{
-            foreach(var Core in Cores){
+        {
+            foreach (var Core in Cores)
+            {
                 Core.Update(gameTime);
 
-                if(Core == Cores[0]){
+                if (Core == Cores[0])
+                {
                     Core.GlobalPosition = Core.LocalPosition;
                     Core.GlobalRotation = Core.LocalRotation;
                     Core.GlobalSize = Core.LocalSize;
-
-                    Console.WriteLine(GlobalPosition);
                 }
-
-                Core.GlobalPosition = Cores[0].LocalPosition + Core.LocalPosition;
-                Core.GlobalRotation += Cores[0].LocalRotation + Core.LocalRotation;
-                Core.GlobalSize += Cores[0].LocalSize + Core.LocalSize;
+                else
+                {
+                    Core.GlobalPosition = Cores[0].LocalPosition + Core.LocalPosition;
+                    Core.GlobalRotation = Cores[0].LocalRotation + Core.LocalRotation;
+                    Core.GlobalSize = Cores[0].LocalSize + Core.LocalSize;
+                }
             }
-		}
+        }
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-		{
-			foreach(var Core in Cores){
+        {
+            foreach (var Core in Cores)
+            {
                 Core.Draw(gameTime, spriteBatch);
             }
-		}
+        }
     }
 }
